f1f450dbd46a4272379774c40d507c1f6d48e9ab
tjd54 <tjd54@DESKTOP-6UCAT56>
2018-08-20 17:38:33 +0900

    오더페이지 추가

--- src/pro/controller/OrderController.java
+++ src/pro/controller/OrderController.java
@@ -4,12 +4,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class WelcomeController {
-	 
-	@RequestMapping({"/index", "/"})
-	public String WelcomeHandle() {
-		return "index";
-		
+@RequestMapping("/order")
+public class OrderController {
+	
+	@RequestMapping("/order")
+	public void OrderHandle01() {
+
 	}
 
 }
