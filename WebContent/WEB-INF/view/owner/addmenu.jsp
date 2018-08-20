decc23f19256632392af76b6b8f188d1e4cef568
yd <yd@DESKTOP-IKF6E22>
2018-08-20 17:58:27 +0900

    메뉴 등록 하는중임

---
+++ WebContent/WEB-INF/view/owner/addmenu.jsp
@@ -0,0 +1,28 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+	pageEncoding="UTF-8"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/functions" prefix="fn"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
+
+
+<title>사장님 페이지</title>
+
+<form>
+	<table>
+		<tr>
+			<td>메뉴:</td>
+				<td><input type="text">
+			</td>
+			
+		</tr>
+		<tr>
+			<td>가격</td>
+			<td><input type="text"></td>
+		</tr>
+		<tr>
+			<td>가게명:</td>
+			<td><input type="text"></td>
+		</tr>
+	</table>
+</form>
+
+
---
+++ src/pro/controller/OwnerController.java
@@ -0,0 +1,47 @@
+package pro.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import pro.dao.AddMenuDao;
+import pro.vo.AddMenuVo;
+
+
+
+@Controller
+@RequestMapping("/owner")
+public class OwnerController {
+	@Autowired
+	AddMenuDao addMenuDao;
+	
+	
+	@GetMapping("/index")
+	public String indexHandle01() {
+		return "owner/index";
+	}
+	
+	
+	
+	@GetMapping("/addmenu")
+	public String addMenuHandle01() {
+		return "owner/addmenu";
+	}
+	
+	@PostMapping("/addmenu")
+	public ModelAndView indexHandle02(@ModelAttribute AddMenuVo vo) {
+		System.out.println(vo.toString());
+		ModelAndView mav = new ModelAndView();
+		boolean r = addMenuDao.addMenu(vo);
+		if(r) {
+			mav.setViewName("index");
+			mav.addObject("success",r);
+		}
+		return mav;
+	}
+}
+
