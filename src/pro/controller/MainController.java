6b0caeaa36f829e2157160b9ef5220a4122db49c
tjd54 <tjd54@DESKTOP-6UCAT56>
2018-08-23 07:37:55 +0900

    음식점 리스트 출력 및 주문하기 페이지 이동

--- src/pro/controller/MainController.java
+++ src/pro/controller/MainController.java
@@ -1,18 +1,30 @@
 package pro.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+
+import pro.dao.StoreDao;
+import pro.vo.StoreVo;
 
 @Controller
 public class MainController {
-	
+
+	@Autowired
+	StoreDao storeDao;
+
 	@RequestMapping("/main")
-	public ModelAndView MainHandle01() {
+	public ModelAndView MainHandle01(@RequestParam String type) {
+		
+		List<StoreVo> list = storeDao.storeLIst(type);
 		
 		ModelAndView mav = new ModelAndView();
 		mav.setViewName("main");
+		mav.addObject("storeList",list);
 		
 		return mav;
 	}
