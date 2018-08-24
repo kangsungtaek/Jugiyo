96a18988089c65268013e7c9b3234ea58d9916ce
Jinne <Jinne@DESKTOP-0IVN4MD>
2018-08-24 19:43:49 +0900

    랜덤추천 mvc

--- src/pro/controller/OrderController.java
+++ src/pro/controller/OrderController.java
@@ -1,11 +1,22 @@
 package pro.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.context.request.WebRequest;
+
+import pro.dao.OrderDao;
+import pro.vo.LogVo;
+import pro.vo.MemberVo;
 
 @Controller
 @RequestMapping("/order")
 public class OrderController {
+	
+	@Autowired
+	OrderDao orderDao;
 	
 	@RequestMapping("/order")
 	public String OrderHandle01() {
@@ -13,8 +24,21 @@ public class OrderController {
 	}
 
 	@RequestMapping("/random")
-	public String randomHandle() {
+	public String randomHandle(WebRequest req) {
+		MemberVo member = (MemberVo) req.getAttribute("vo", WebRequest.SCOPE_SESSION);
+		List<LogVo> list = orderDao.findLog(member.getId());
+		int num = 0;
+		int[] set = new int[10];
 		
-		return null;
+		for(int i=1; i<11; i++) {
+			for(LogVo l : list) {
+				if(i == l.getType())
+					continue;
+				else
+					set[i] = i;
+			}
+		}
+		num = set[1 + (int)Math.random()*set.length];
+		return "main?no="+num;
 	}
 }
--- src/pro/vo/LogVo.java
+++ src/pro/vo/LogVo.java
@@ -6,6 +6,7 @@ import java.util.Date;
 public class LogVo {	
 	String id;
 	String store; //음식점 no
+	int type; //음식점의 타입
 	String[] menu;
 	Date date;
 	
@@ -20,6 +21,12 @@ public class LogVo {
 	}
 	public void setStore(String store) {
 		this.store = store;
+	}
+	public int getType() {
+		return type;
+	}
+	public void setType(int type) {
+		this.type = type;
 	}
 	public String[] getMenu() {
 		return menu;
@@ -36,7 +43,7 @@ public class LogVo {
 	
 	@Override
 	public String toString() {
-		return "LogVo [id=" + id + ", store=" + store + ", menu=" + Arrays.toString(menu) + ", date=" + date + "]";
+		return "LogVo [id=" + id + ", store=" + store + ", type=" + type + ", menu=" + Arrays.toString(menu) + ", date=" + date + "]";
 	}
 	
 }
