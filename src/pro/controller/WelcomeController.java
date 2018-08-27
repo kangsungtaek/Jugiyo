4a5690be068c5b61ba29565f6aa9e92fd08af269
Jinne <Jinne@DESKTOP-0IVN4MD>
2018-08-27 16:14:19 +0900

    푸쉬전

--- src/pro/controller/WelcomeController.java
+++ src/pro/controller/WelcomeController.java
@@ -1,8 +1,6 @@
 package pro.controller;
 
-import javax.servlet.ServletContext;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
