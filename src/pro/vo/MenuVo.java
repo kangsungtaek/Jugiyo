8c123b671bbbe2b7e352ebf7ab04ccd9dd5c9212
tjd54 <tjd54@DESKTOP-6UCAT56>
2018-08-30 19:29:40 +0900

    ㅁㄴㅇ

--- src/pro/vo/MenuVo.java
+++ src/pro/vo/MenuVo.java
@@ -11,70 +11,92 @@ public class MenuVo implements Serializable {
 	String name;
 	int price;
 	int store;
+
 	int fileNo;
 	String fileUri;
 	String fileName;
+
 	int type;
+
+
 	MultipartFile[] attach;
-	public int getNo() {
-		return no;
-	}
-	public void setNo(int no) {
-		this.no = no;
-	}
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public int getPrice() {
-		return price;
-	}
-	public void setPrice(int price) {
-		this.price = price;
-	}
-	public int getStore() {
-		return store;
-	}
-	public void setStore(int store) {
-		this.store = store;
-	}
+
+	// 강성택 메뉴 카운트 추가
+	int cnt;
+	
 	public int getFileNo() {
 		return fileNo;
 	}
+
 	public void setFileNo(int fileNo) {
 		this.fileNo = fileNo;
 	}
-	public String getFileUri() {
-		return fileUri;
-	}
-	public void setFileUri(String fileUri) {
-		this.fileUri = fileUri;
-	}
-	public String getFileName() {
-		return fileName;
-	}
-	public void setFileName(String fileName) {
-		this.fileName = fileName;
-	}
+
 	public int getType() {
 		return type;
 	}
+
 	public void setType(int type) {
 		this.type = type;
 	}
+
+
+
+	
 	public MultipartFile[] getAttach() {
 		return attach;
 	}
+
 	public void setAttach(MultipartFile[] attach) {
 		this.attach = attach;
 	}
+
+	public int getCnt() {
+		return cnt;
+	}
+
+	public void setCnt(int cnt) {
+		this.cnt = cnt;
+	}
+
+	public int getStore() {
+		return store;
+	}
+
+	public void setStore(int store) {
+		this.store = store;
+	}
+
+	public int getNo() {
+		return no;
+	}
+
+	public void setNo(int no) {
+		this.no = no;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getPrice() {
+		return price;
+	}
+
+	public void setPrice(int price) {
+		this.price = price;
+	}
+
+
 	@Override
 	public String toString() {
-		return "MenuVo [no=" + no + ", name=" + name + ", price=" + price + ", store=" + store + ", fileNo=" + fileNo
-				+ ", fileUri=" + fileUri + ", fileName=" + fileName + ", type=" + type + ", attach="
-				+ Arrays.toString(attach) + "]";
+		return "MenuVo [no=" + no + ", name=" + name + ", price=" + price + ", store=" + store + ", type=" + type
+				+ ", attach=" + Arrays.toString(attach) + ", cnt=" + cnt + "]";
 	}
+
 	
 }
